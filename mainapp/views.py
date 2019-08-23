aefbe8603a1cc997b4b09f2679b97ccb52caa8fd
shatimur <shatimur@ya.ru>
2019-08-23 11:51:10 +0300

    ДЗ первого урока - корректный набор файлов

---
+++ mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def main(request):
+    return render(request, 'index.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
