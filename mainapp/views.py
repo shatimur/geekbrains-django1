754944965f5cd4311a45292d606874cf8e5219b0
shatimur <shatimur@ya.ru>
2019-08-26 17:32:24 +0300

    json for contact added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 
 
@@ -28,29 +29,36 @@ def products(request):
 
 
 def contact(request):
-    locations = [
-        {
-            'city': 'Москва',
-            'phone': '+7-888-888-8888',
-            'email': 'info@geekshop-center.ru',
-            'address':'В пределах МКАД'
-        },
-        {
-            'city': 'Санкт-Петербург',
-            'phone': '+7-666-888-6666',
-            'email': 'info@geekshop-nw.ru',
-            'address': 'В пределах 100 км'
-        },
-        {
-            'city': 'Казань',
-            'phone': '+7-999-888-9999',
-            'email': 'info@geekshop-tat.ru',
-            'address': 'В пределах центра'
-        }
-    ]
+    # locations = [
+    #     {
+    #         'city': 'Москва',
+    #         'phone': '+7-888-888-8888',
+    #         'email': 'info@geekshop-center.ru',
+    #         'address': 'В пределах МКАД'
+    #     },
+    #     {
+    #         'city': 'Санкт-Петербург',
+    #         'phone': '+7-666-888-6666',
+    #         'email': 'info@geekshop-nw.ru',
+    #         'address': 'В пределах 100 км'
+    #     },
+    #     {
+    #         'city': 'Казань',
+    #         'phone': '+7-999-888-9999',
+    #         'email': 'info@geekshop-tat.ru',
+    #         'address': 'В пределах центра'
+    #     }
+    # ]
+    # with open('geekshop/locations.json', 'w', encoding='utf-8') as f:
+    #     json.dump(locations, f)
+
+    with open('geekshop/locations.json', 'r', encoding='utf-8') as f:
+        locations = json.load(f)
 
     context = {
         'title': 'Контакты',
         'locations': locations
     }
+
+
     return render(request, 'contact.html', context=context)
