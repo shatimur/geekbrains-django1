53807685a4b92782cfa51b97a0d1b601b9af7523
shatimur <shatimur@ya.ru>
2019-08-28 22:44:09 +0300

    lesson3 models

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,13 @@
 import json
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 
 # Create your views here.
 
 
 def main(request):
+    products = Product.objects.all()
     context = {
         'title': 'Главная'
     }
