e8d1c27f6ee4c593426ff909694e87d4083df429
shatimur <shatimur@ya.ru>
2019-09-04 22:44:24 +0300

    lesson4 fill_db working

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя',
-                            max_length=128,
+                            max_length=64,
                             unique=True)
     description = models.TextField(verbose_name='описание',
                                    blank=True)
