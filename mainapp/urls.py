2d844909187f92f842e3d8460a79efb26a95b218
shatimur <shatimur@ya.ru>
2019-09-09 15:54:17 +0300

    lesson5 basketapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 import mainapp.views as mainapp
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
 ]
+
